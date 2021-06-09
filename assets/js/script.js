ba86ed70d0c6c554e1450b585dba020ed9cb6257
Ben Thackray <mthack92@gmail.com>
2021-06-09 07:06:56 -0400

    added html, css and js files. Began to edit html and css

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// Start page with button that begins the quiz
+
+// User clicks button. Quiz starts
+
+// Title page disappears once quiz is started. 
+
+//Quiz starts, timer starts with 15 seconds per question. There will be 5 questions total making this 75 seconds remaining
+
+//Make sure timer subtracts 1 second per second
+
+//User is presented with a question along with 4 answers presented as buttons
+
+// As questions are answered with button clicks, the next question replaces them on the screen
+
+// After a question is answered, either "wrong" or "correct" will be displayed below the next question
+
+// If an answer is wrong, fifteen seconds are taken off the timer.
+
+// If the timer goes down to zero, the quiz ends and a game over screen appears
+
+// After the last question is answered, the time stops 
+
+//The remaining time left appears as the score
+
+// The user can submit their initials
+
+// Initials are placed in a high score area
+
+// On the high scores screen, there is option to clear the high scores
+
+// Go back button that takes user back to Start Menu
